19715ec9de7d8d9207591b4b39bdcd2df31089ed
Raja Luqman Sarwar <luqmanrajput20@gmail.com>
2022-08-05 02:56:54 -0700

    refactoring

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,28 +4,36 @@ const User = require('../models/User')
 const {body,validationResult}=require('express-validator');
 
 // Create a user using: POST "/api/auth/createuser". No login required
-router.post('/',
+router.post('/createuser',
     [body('name',"Invalid name").isLength({ min: 3 }),
     body('email',"Invalid email").isEmail(),
     body('password',"Invalid password").isLength({ min: 5 })]
     , async(req,res)=>{
-        // Handling error messages
+        // if there are errors, return errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
           return res.status(400).json({ errors: errors.array() });
         }
-        let user=User.findOne({email:req.body.email})
-        if(user){
-            return res.status(400).json({error:"User with this email already exits"})
+        // checking if email already exists
+        else{
+            try {
+                let user=await User.findOne({email:req.body.email})
+                if(user){
+                    return res.status(400).json({error:"User with this email already exits"})
+                }
+                // creating user
+                else{
+                    user = await User.create({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password,
+                    })
+                    res.json(user)
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
         }
-            user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-          })
-        //   .then(user => res.json(user))
-        //   .catch(err=> {console.log(err)
-        //     res.json({error:"Invalid email",message:err.message})} )
           
 })
 
