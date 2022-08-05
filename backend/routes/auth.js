8d5dadcc2cddfe3d4570837f369db43e29f84954
Raja Luqman Sarwar <luqmanrajput20@gmail.com>
2022-08-05 02:30:37 -0700

    Final backend setup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
+const {body,validationResult}=require('express-validator');
 
-// Create a user using: POST "/api/auth/". Doesn't require Auth
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user= User(req.body);
-    user.save();
-    res.send(req.body);
+// Create a user using: POST "/api/auth/createuser". No login required
+router.post('/',
+    [body('name',"Invalid name").isLength({ min: 3 }),
+    body('email',"Invalid email").isEmail(),
+    body('password',"Invalid password").isLength({ min: 5 })]
+    , async(req,res)=>{
+        // Handling error messages
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        let user=User.findOne({email:req.body.email})
+        if(user){
+            return res.status(400).json({error:"User with this email already exits"})
+        }
+            user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+          })
+        //   .then(user => res.json(user))
+        //   .catch(err=> {console.log(err)
+        //     res.json({error:"Invalid email",message:err.message})} )
+          
 })
 
 module.exports = router;
